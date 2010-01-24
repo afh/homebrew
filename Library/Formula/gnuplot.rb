021cd33131a876679ca088a4e2b0a96c1ef02ad1
Alexis Hildebrandt <afh@surryhill.net>
2010-01-24 16:47:05 +0100

    Update Formula: gnuplot. Version bump.
    Thanks to MrMage for the version bump.
    http://github.com/mxcl/homebrew/issues#issue/325.
    
    This commit also adds aquaterm as an optional depends_on
    and disables wxwidget support.

--- Library/Formula/gnuplot.rb
+++ Library/Formula/gnuplot.rb
@@ -1,14 +1,34 @@
 require 'formula'
 
 class Gnuplot <Formula
-  @url='http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.2.6/gnuplot-4.2.6.tar.gz'
+  @url='http://downloads.sourceforge.net/project/gnuplot/gnuplot/4.4.0-rc1/gnuplot-4.4.0-rc1.tar.gz'
   @homepage='http://www.gnuplot.info/'
-  @md5='c10468d74030e8bed0fd6865a45cf1fd'
-  
+  @md5='b7be3d0e812dc309639a28dd1e5a9172'
+
   depends_on 'readline'
+ #depends_on 'wxmac'
+  depends_on 'jpeg' => :optional
+  depends_on 'gd' => :optional
+  depends_on 'aquaterm' => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-readline=#{prefix}/lib"
+    configure_args = [
+        "--prefix=#{prefix}",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--disable-wxwidgets",
+        "--with-readline=#{prefix}/lib",
+        "--enable-datastrings",
+        "--enable-histograms"
+    ]
+    system "./configure", *configure_args
     system "make install"
+  end
+
+  def caveats
+    <<-EOF
+    If you want support for aquaterm on Mac OS X,
+    check the aquaterm caveats beforehand.
+    EOF
   end
 end
